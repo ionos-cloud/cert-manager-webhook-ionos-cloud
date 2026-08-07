d89e1ba1ee3a337d3e3ceb084ed81ab02f444f0b
zak905 <zakaria.amine88@gmail.com>
2026-08-07 10:51:29 +0200

    remove accidentally committed code

--- cmd/webhook/main_test.go
+++ cmd/webhook/main_test.go
@@ -14,12 +14,6 @@ import (
 var zone = os.Getenv("TEST_ZONE_NAME")
 
 func TestBasicConformance(t *testing.T) {
-	zone = "ionos-cloud-cdn.de"
-	os.Setenv("KUBEBUILDER_ASSETS", "/home/zakaria/Desktop/myWork/ionos/workspace/cert-manager-webhook-ionos-cloud/bin/tools/k8s/1.36.2-linux-amd64")
-	os.Setenv("TEST_ASSET_ETCD", "/home/zakaria/Desktop/myWork/ionos/workspace/cert-manager-webhook-ionos-cloud/bin/tools/etcd")
-	os.Setenv("TEST_ASSET_KUBE_APISERVER", "/home/zakaria/Desktop/myWork/ionos/workspace/cert-manager-webhook-ionos-cloud/bin/tools/kube-apiserver")
-	os.Setenv("TEST_ASSET_KUBECTL", "/home/zakaria/Desktop/myWork/ionos/workspace/cert-manager-webhook-ionos-cloud/bin/tools/kubectl")
-
 	if zone == "" {
 		t.Fatal("TEST_ZONE_NAME environment variable must be set before running the test")
 	}
